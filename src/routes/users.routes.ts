30500c0aab69183cd1f813f94399146fa2d74a21
William Pereira <williamcpereira89@gmail.com>
2020-06-14 14:06:56 -0300

    Upload Avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import isAuthenticated from '../middlewares/isAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request: Request, response: Response) => {
     try {
@@ -22,8 +27,19 @@ usersRouter.post('/', async (request: Request, response: Response) => {
 usersRouter.patch(
     '/avatar',
     isAuthenticated,
+    upload.single('avatar'),
     async (request: Request, response: Response) => {
-        return response.json();
+        try {
+            const avatarService = new UpdateUserAvatarService();
+            const user = await avatarService.execute({
+                userId: request.userId,
+                avatarFilename: request.file.filename
+            });
+
+            return response.json(user);
+        } catch (error) {
+            return response.status(400).json({ message: error.message });
+        }
     }
 );
 
---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+import uploadConfig from '../config/upload';
+
+interface RequestDTO {
+    userId: string;
+    avatarFilename: string;
+}
+export default class UpdateUserAvatarService {
+    public async execute({
+        userId,
+        avatarFilename
+    }: avatarFilename): Promise<User> {
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne(userId);
+
+        if (!user) {
+            throw new Error('Only authenticated users can change avatar.');
+        }
+
+        if (user.avatar) {
+            const userAvatarFilePath = path.join(
+                uploadConfig.directory,
+                user.avatar
+            );
+
+            const userFileExists = await fs.promises.stat(userAvatarFilePath);
+
+            if (userFileExists) {
+                await fs.promises.unlink(userAvatarFilePath);
+            }
+        }
+
+        user.avatar = avatarFilename;
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
